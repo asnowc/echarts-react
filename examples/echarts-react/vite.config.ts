bca5027fae6812384e88b7a1ea701aedb9665a97
Eavid <ganrn@vip.qq.com>
2024-05-29 19:47:08 +0800

    chore: 更新示例

---
+++ examples/echarts-react/vite.config.ts
@@ -0,0 +1,13 @@
+import type { UserConfig } from "vite";
+
+const dir = (import.meta as any).dirname!;
+const imports = {
+  "echarts-comp": "./node_modules/echarts-comp/src/mod.ts",
+  react: "https://esm.sh/react@18",
+  "react-dom/": "https://esm.sh/react-dom@18/",
+  echarts: "https://esm.sh/echarts@5?bundle",
+};
+
+export default {
+  root: ".",
+} as UserConfig;
