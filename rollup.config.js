9c932c541755f6da29b60cbbc25da72ab9dfb31a
Eavid <ganrn@vip.qq.com>
2024-06-01 09:05:14 +0800

    build: 不在输出 cjs

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,11 @@ import tsPlugin from "@rollup/plugin-typescript";
 
 export default defineConfig({
   input: "./src/mod.ts",
-  output: [
-    {
-      minifyInternalExports: false,
-      format: "cjs",
-      file: "dist/mod.cjs",
-    },
-    {
-      minifyInternalExports: false,
-      format: "es",
-      file: "dist/mod.js",
-    },
-  ],
+  output: {
+    minifyInternalExports: false,
+    format: "es",
+    file: "dist/mod.js",
+  },
   external: ["react", "echarts"],
   plugins: [
     tsPlugin({
